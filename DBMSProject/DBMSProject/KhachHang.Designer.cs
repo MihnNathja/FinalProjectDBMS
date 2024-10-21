2000068e3ae46731d0f3701777e900a6f443e63a
VuongQue <lapque.hcmute@gmail.com>
2024-10-21 19:56:42 +0700

    creat KhachHang.cs Dichvu.cs XemHoaDon.cs

---
+++ DBMSProject/DBMSProject/KhachHang.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace DBMSProject
+{
+    partial class KhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtServiceFee = new System.Windows.Forms.TextBox();
+            this.txtRemainTime = new System.Windows.Forms.TextBox();
+            this.txtUseTime = new System.Windows.Forms.TextBox();
+            this.lblServiceFee = new System.Windows.Forms.Label();
+            this.lblRemainTime = new System.Windows.Forms.Label();
+            this.lblUseTime = new System.Windows.Forms.Label();
+            this.lblTotalTime = new System.Windows.Forms.Label();
+            this.btnDichVu = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblUserName);
+            this.groupBox1.Controls.Add(this.txtServiceFee);
+            this.groupBox1.Controls.Add(this.txtRemainTime);
+            this.groupBox1.Controls.Add(this.txtUseTime);
+            this.groupBox1.Controls.Add(this.lblServiceFee);
+            this.groupBox1.Controls.Add(this.lblRemainTime);
+            this.groupBox1.Controls.Add(this.lblUseTime);
+            this.groupBox1.Controls.Add(this.lblTotalTime);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(351, 172);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblUserName.Location = new System.Drawing.Point(4, -1);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(87, 19);
+            this.lblUserName.TabIndex = 10;
+            this.lblUserName.Text = "UserName:";
+            // 
+            // txtServiceFee
+            // 
+            this.txtServiceFee.Location = new System.Drawing.Point(165, 112);
+            this.txtServiceFee.Margin = new System.Windows.Forms.Padding(4);
+            this.txtServiceFee.Name = "txtServiceFee";
+            this.txtServiceFee.ReadOnly = true;
+            this.txtServiceFee.Size = new System.Drawing.Size(178, 22);
+            this.txtServiceFee.TabIndex = 9;
+            // 
+            // txtRemainTime
+            // 
+            this.txtRemainTime.Location = new System.Drawing.Point(165, 70);
+            this.txtRemainTime.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRemainTime.Name = "txtRemainTime";
+            this.txtRemainTime.ReadOnly = true;
+            this.txtRemainTime.Size = new System.Drawing.Size(178, 22);
+            this.txtRemainTime.TabIndex = 7;
+            // 
+            // txtUseTime
+            // 
+            this.txtUseTime.Location = new System.Drawing.Point(165, 34);
+            this.txtUseTime.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUseTime.Name = "txtUseTime";
+            this.txtUseTime.ReadOnly = true;
+            this.txtUseTime.Size = new System.Drawing.Size(178, 22);
+            this.txtUseTime.TabIndex = 6;
+            // 
+            // lblServiceFee
+            // 
+            this.lblServiceFee.AutoSize = true;
+            this.lblServiceFee.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblServiceFee.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblServiceFee.Location = new System.Drawing.Point(15, 112);
+            this.lblServiceFee.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblServiceFee.Name = "lblServiceFee";
+            this.lblServiceFee.Size = new System.Drawing.Size(127, 19);
+            this.lblServiceFee.TabIndex = 4;
+            this.lblServiceFee.Text = "Chi phí dịch vụ:";
+            // 
+            // lblRemainTime
+            // 
+            this.lblRemainTime.AutoSize = true;
+            this.lblRemainTime.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblRemainTime.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblRemainTime.Location = new System.Drawing.Point(15, 73);
+            this.lblRemainTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRemainTime.Name = "lblRemainTime";
+            this.lblRemainTime.Size = new System.Drawing.Size(139, 19);
+            this.lblRemainTime.TabIndex = 2;
+            this.lblRemainTime.Text = "Thời gian còn lại:";
+            // 
+            // lblUseTime
+            // 
+            this.lblUseTime.AutoSize = true;
+            this.lblUseTime.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblUseTime.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblUseTime.Location = new System.Drawing.Point(15, 34);
+            this.lblUseTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUseTime.Name = "lblUseTime";
+            this.lblUseTime.Size = new System.Drawing.Size(142, 19);
+            this.lblUseTime.TabIndex = 1;
+            this.lblUseTime.Text = "Thời gian bắt đầu:";
+            // 
+            // lblTotalTime
+            // 
+            this.lblTotalTime.AutoSize = true;
+            this.lblTotalTime.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblTotalTime.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTotalTime.Location = new System.Drawing.Point(41, 32);
+            this.lblTotalTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalTime.Name = "lblTotalTime";
+            this.lblTotalTime.Size = new System.Drawing.Size(0, 19);
+            this.lblTotalTime.TabIndex = 0;
+            // 
+            // btnDichVu
+            // 
+            this.btnDichVu.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnDichVu.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDichVu.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDichVu.Location = new System.Drawing.Point(21, 211);
+            this.btnDichVu.Name = "btnDichVu";
+            this.btnDichVu.Size = new System.Drawing.Size(149, 32);
+            this.btnDichVu.TabIndex = 2;
+            this.btnDichVu.Text = "Dịch vụ";
+            this.btnDichVu.UseVisualStyleBackColor = false;
+            this.btnDichVu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(198, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Xem Hoá Đơn";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(197, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Đăng xuất";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(20, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Đổi mật khẩu";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 321);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(335, 320);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // KhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 665);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDichVu);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "KhachHang";
+            this.Text = "KhachHang";
+            this.Load += new System.EventHandler(this.KhachHang_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtServiceFee;
+        private System.Windows.Forms.TextBox txtRemainTime;
+        private System.Windows.Forms.TextBox txtUseTime;
+        private System.Windows.Forms.Label lblServiceFee;
+        private System.Windows.Forms.Label lblRemainTime;
+        private System.Windows.Forms.Label lblUseTime;
+        private System.Windows.Forms.Label lblTotalTime;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Button btnDichVu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
