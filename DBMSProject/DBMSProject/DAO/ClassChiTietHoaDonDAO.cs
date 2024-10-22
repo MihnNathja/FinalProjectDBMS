27ef51db6fa715a11d071c0bb6a3fac12289ba45
luaan <luandophu8114@gmail.com>
2024-10-22 10:02:01 +0700

    Add Object and DAO

---
+++ DBMSProject/DBMSProject/DAO/ClassChiTietHoaDonDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBMSProject.DAO
+{
+    internal class ClassChiTietHoaDonDAO
+    {
+        SqlConnection conn = new SqlConnection(Properties.Settings.Default.connStr);
+        /*private void testKetNoi()
+        {
+            try
+            {
+                conn.Open();
+                *//*string sqlStr = string.Format("SELECT *FROM HocSinh”);
+               
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlStr, conn);
+                DataTable dtSinhVien = new DataTable();
+                adapter.Fill(dtSinhVien);
+                gvHsinh.DataSource = dtSinhVien; /// gvHsinh = name cua data gridview*//*
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }*/
+    }
+}
+
